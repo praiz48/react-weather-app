0ae94408a7f4ea1cd7e2d653a4a6e6f02c362a47
praise48 <praiz8015@gmail.com>
2025-04-15 16:09:33 +0100

    Refactor weather data handling to use a single state object for improved readability and maintainability and added loading message

--- weather app/src/App.jsx
+++ weather app/src/App.jsx
@@ -3,12 +3,15 @@ import Display from "./components/Display";
 import { useState } from "react";
 function App() {
   const api_key = "d8603accd66c58cd8ba882ea4ce81efe";
-  const [weather, setWeather] = useState("unknown");
-  const [temperature, setTemperature] = useState(0);
-  const [icon, setIcon] = useState(
-    "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png"
-  );
-  const [country, setCountry] = useState("no where");
+  const [loading, setLoading] = useState(false);
+
+  const [weatherData, setWeatherData] = useState({
+    weather: "unknown",
+    temperature: 0,
+    icon: "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png",
+    country: "no where",
+  });
+
   const onSearch = (search) => {
     if (!search || search.trim() === "") {
       alert("Please enter a city name");
@@ -18,32 +21,38 @@ function App() {
   };
   async function fetchWeatherData(city) {
     try {
+      setLoading(true);
       const response = await fetch(
         `http://api.weatherstack.com/current?access_key=${api_key}&query=${city}`
       );
       if (response.ok) {
         const data = await response.json();
-        setWeather(data.current.weather_descriptions[0]);
-        setTemperature(data.current.temperature);
-        setIcon(data.current.weather_icons[0]);
-        setCountry(data.location.country);
+        setWeatherData({
+          weather: data.current.weather_descriptions[0],
+          temperature: data.current.temperature,
+          icon: data.current.weather_icons[0],
+          country: data.location.country,
+        });
       } else {
         throw new Error("Network response was not ok");
       }
     } catch (error) {
       console.error("Error fetching weather data:", error);
       alert("Please enter a city name");
+    } finally {
+      setLoading(false); // Done loading
     }
   }
   return (
     <>
       <h1>hi</h1>
       <Searching onButtonClick={onSearch} />
+      {loading && <p>Loading...</p>}
       <Display
-        Weather={weather}
-        Temp={temperature}
-        Icon={icon}
-        Country={country}
+        Weather={weatherData.weather}
+        Temp={weatherData.temperature}
+        Icon={weatherData.icon}
+        Country={weatherData.country}
       />
     </>
   );
